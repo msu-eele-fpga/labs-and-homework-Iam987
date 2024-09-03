1792fb4404e189951cb55f8f4b2fe831a7b033c2
Ian Crittenden <iancrittenden@rocketmail.com>
2024-09-03 13:58:18 -0600

    Write functionallity for the 2-flip-flop synchronizer

---
+++ hdl/synchronizer/synchronizer.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity synchronizer is
+port (
+	clk : in std_ulogic;
+	async : in std_ulogic;
+	sync : out std_ulogic;
+);
+
+architecture synchronizer_arch of synchronizer is
+
+	signal Q1 : std_ulogic;
+
+	begin 
+	process(clk)
+		begin 
+		if (rising_edge(clk)) then
+			Q1 <= async;
+			sync <= Q1;
+		end if;
+	end process;
+end architecture;
