deb0dfadc53d32788542e5ce90f2cbcaf9348a84
Iam987 <iancrittenden@rocketmail.com>
2024-09-05 14:04:10 -0600

    Simulate the testbench and confirm output transcript is correct

--- hdl/synchronizer/synchronizer.vhd
+++ hdl/synchronizer/synchronizer.vhd
@@ -5,8 +5,9 @@ entity synchronizer is
 port (
 	clk : in std_ulogic;
 	async : in std_ulogic;
-	sync : out std_ulogic;
+	sync : out std_ulogic
 );
+end entity;
 
 architecture synchronizer_arch of synchronizer is
 
