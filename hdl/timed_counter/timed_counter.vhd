78a9550c12ff4be2efe1b913dca4baff21f0926f
Iam987 <iancrittenden@rocketmail.com>
2024-09-09 23:49:32 -0600

    Complete hw 4 worksheet hopfully

--- hdl/timed_counter/timed_counter.vhd
+++ hdl/timed_counter/timed_counter.vhd
@@ -14,7 +14,27 @@ entity timed_counter is
 end entity;
 
 architecture timed_counter_arch of timed_counter is
-
+	
+	constant COUNTER_LIMIT : integer := count_time / clk_period;
+	
+	signal count : integer range 0 to COUNTER_LIMIT;
+	
 	begin
 	
+	COUNT_proc : process (clk, enable) is
+		begin 
+		if (enable = false) then
+			count <= 0;
+			done <= false;
+		elsif (rising_edge(clk))then
+			if (count = COUNTER_LIMIT) then
+				count <= 0;
+				done <= true;
+			elsif (count = 0) then
+				count <= count + 1;
+				done <= false;
+			else count <= count + 1;
+			end if;
+		end if;	
+	end process;	
 end architecture;
--- sim/timed-counter/timed_counter_tb.vhd
+++ sim/timed-counter/timed_counter_tb.vhd
@@ -25,7 +25,11 @@ architecture testbench of timed_counter_tb is
 	signal enable_100ns_tb 	: boolean := false;
 	signal done_100ns_tb	: boolean;
 	
+	signal enable_260ns_tb 	: boolean := false;
+	signal done_260ns_tb	: boolean;
+	
 	constant HUNDRED_NS	: time := 100 ns;
+	constant TWOSIXTY_NS	: time := 260 ns;
 	
 	procedure predict_counter_done (
 		constant count_time : in time;
@@ -56,6 +60,15 @@ architecture testbench of timed_counter_tb is
 				done 	=> done_100ns_tb);
 		clk_tb <= not clk_tb after CLK_PERIOD / 2;
 		
+		dut_260ns_counter	: component timed_counter
+			generic map (
+				clk_period => CLK_PERIOD,
+				count_time => TWOSIXTY_NS)
+			port map(
+				clk 	=> clk_tb,
+				enable 	=> enable_260ns_tb,
+				done 	=> done_260ns_tb);
+		
 		stimuli_and_checker : process is
 			begin
 			
@@ -69,6 +82,66 @@ architecture testbench of timed_counter_tb is
 					wait_for_clock_edge(clk_tb);
 					predict_counter_done(HUNDRED_NS, enable_100ns_tb, done_100ns_tb, i); -- test whether the counter's done output is correct or not
 				end loop;
+				print("test done");
+				
+				-- Test for no assersion of done when disabled for two count_time periods (100 ns)
+				print("testing disabled for two count_time periods (100 ns)");
+				wait_for_clock_edge(clk_tb);
+				enable_100ns_tb <= false;
+				for i in 0 to (2 * (HUNDRED_NS / CLK_PERIOD)) Loop
+					wait_for_clock_edge(clk_tb);
+					predict_counter_done(HUNDRED_NS, enable_100ns_tb, done_100ns_tb, i);
+				end loop;
+				print("test done");
+				
+				-- Test for correct done toggling for multiple enabled count_time periods (100 ns)
+				print("testing enabled for multiple count_time periods (100 ns)");
+				wait_for_clock_edge(clk_tb);
+				enable_100ns_tb <= true;
+				for j in 0 to 5 Loop
+					for i in 0 to (HUNDRED_NS / CLK_PERIOD) Loop
+						wait_for_clock_edge(clk_tb);
+						predict_counter_done(HUNDRED_NS, enable_100ns_tb, done_100ns_tb, i);
+					end loop;
+				end loop;
+				print("test done");
+				
+				
+				------------------------------------------------------------------------------------------
+				-- test 260 ns timer when it's enabled
+				print("testing 260 ns timer: enabled");
+				wait_for_clock_edge(clk_tb);
+				enable_260ns_tb <= true;
+				
+				-- Loop for the number of clock cycles that is equal to the timer's period
+				for i in 0 to (TWOSIXTY_NS / CLK_PERIOD) Loop
+					wait_for_clock_edge(clk_tb);
+					predict_counter_done(TWOSIXTY_NS, enable_260ns_tb, done_260ns_tb, i); -- test whether the counter's done output is correct or not
+				end loop;
+				print("test done");
+				
+				-- Test for no assersion of done when disabled for two count_time periods (260 ns)
+				print("testing disabled for two count_time periods (260 ns)");
+				wait_for_clock_edge(clk_tb);
+				enable_260ns_tb <= false;
+				for i in 0 to (2 * (TWOSIXTY_NS / CLK_PERIOD)) Loop
+					wait_for_clock_edge(clk_tb);
+					predict_counter_done(TWOSIXTY_NS, enable_260ns_tb, done_260ns_tb, i);
+				end loop;
+				print("test done");
+				
+				-- Test for correct done toggling for multiple enabled count_time periods (260 ns)
+				print("testing enabled for multiple count_time periods (260 ns)");
+				wait_for_clock_edge(clk_tb);
+				enable_260ns_tb <= true;
+				for j in 0 to 5 Loop
+					for i in 0 to (TWOSIXTY_NS / CLK_PERIOD) Loop
+						wait_for_clock_edge(clk_tb);
+						predict_counter_done(TWOSIXTY_NS, enable_260ns_tb, done_260ns_tb, i);
+					end loop;
+				end loop;
+				print("test done");
+				
 				
 				-- add other test cases here
 				
