52d77ed6f17ecc617c4339c7211fd76f34e47d68
Iam987 <iancrittenden@rocketmail.com>
2024-09-13 09:23:26 -0600

    HW 6 - Add files for one_pulse testing

---
+++ hdl/async-conditioner/one_pulse.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity one_pulse is
+	port (
+		clk		: in std_ulogic;
+		rst		: in std_ulogic;
+		input	: in std_ulogic;
+		pulse	: out std_ulogic);
+end entity;
+
+architecture one_pulse_arch of one_pulse is
+	
+	begin
+	
+end architecture;
