a62cd0cbffbad7fb6de6bdb0fd0edd122ba83ed8
Iam987 <iancrittenden@rocketmail.com>
2024-11-04 22:58:52 -0700

    Lab 8 - Start creating led_patterns.c

---
+++ sw/led-patterns/led_patterns.c
@@ -0,0 +1,15 @@
+/* Name: Ian Crittenden
+ * SoC FPGAs I - Trevor Vannoy
+ * 11/04/24
+ * SW Led Pattern Controller
+ * This program will control the leds with various patterns when software control mode is enabled in the fpga via the lightweight bus
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int main(void){
+	system("echo devmem1 0xff200000 0x0
+}
