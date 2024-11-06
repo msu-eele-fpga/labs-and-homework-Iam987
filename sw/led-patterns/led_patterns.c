5af35351ff0834585f9a2dde24bbf20d02572e63
Ian Crittenden <iancrittenden@rocketmail.com>
2024-11-05 21:47:36 -0700

    Lab 8 - Get -p inputs read and stored correctly

--- sw/led-patterns/led_patterns.c
+++ sw/led-patterns/led_patterns.c
@@ -13,14 +13,14 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <sys/mman.h> //for mman?
+#include <sys/mman.h> //for mman
 #include <fcntl.h> //for file open flags
 #include <unistd.h> //for getting the page size
 #include <getopt.h> //for handling options
 #include <signal.h> //for handling ^C
 
 void usage(){
-	fprintf(stderr, "\n\nled_patterns: led_patterns [-options] [filename/pattern+durration,...]\n");
+	fprintf(stderr, "\n\nled_patterns: led_patterns [-options] [filename/pattern durration,...]\n");
 	fprintf(stderr, "\tControl the leds on the de10nano with software\n");
 	fprintf(stderr, "\n\tWithout arguments, `led_patterns` will display this help text\n");
 	fprintf(stderr, "\nOptions:\n"
@@ -38,66 +38,101 @@ void dvmemusage(){
 	fprintf(stderr, " VALUE The optional value to write to ADDRESS; if not given, a read will be performed.\n\n");
 }
 
-int devmem(char *Addr, char *val){
+int devmem(char *Addr, char *val){ // I know this function is very ugly, I didn't want to refactor it from lab 7 so here it is
 	//This is the size of the page of memory in the system. (4096 bytes probably)
 	const size_t PAGE_SIZE = sysconf(_SC_PAGE_SIZE);
-	
-	
-	//If value arg given, perform a write opp
-	bool is_write = true;
 	
 	const uint32_t ADDRESS = strtoul(Addr, NULL, 0);
 	
 	int fd = open("/dev/mem", O_RDWR | O_SYNC);
 	if (fd == -1){
-		//fprintf(stderr, "failed to open /dev/mem.\n");
+		fprintf(stderr, "failed to open /dev/mem.\n");
 		return 1;
 	}
 	
 	//mmap needs to map memory at page boundaries; 
 	uint32_t page_aligned_addr = ADDRESS & ~(PAGE_SIZE - 1);
-	//printf("memory addresses:\n");
-	//printf("---------------------------------------------------------------------------\n");
-	//printf("page aligned address = 0x%x\n",page_aligned_addr);
 	
 	//map a page of physical memory into virtual memory. https://www.man7.org/linux/man-pages/man2/mmap.2.html
 	uint32_t *page_virtual_addr = (uint32_t *)mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, page_aligned_addr);
 	if(page_virtual_addr == MAP_FAILED){
-	       // fprintf(stderr, "failed to map memory.\n");
+	       fprintf(stderr, "failed to map memory.\n");
 	        return 1;
 	}	
-	//printf("page_virtual_addr = %p\n", page_virtual_addr);
 	
 	//The address we want to access might not be page-aligned. Since we mapped a page-aligned address, we need our target address' offet from the page boundary. Using this offset, we can copute the virtual address corresponding to our physical target address (ADDRESS).
 	uint32_t offset_in_page = ADDRESS & (PAGE_SIZE - 1);
-	//printf("offset in page = 0x%x\n",offset_in_page);
 	
 	//Compute the virtual address corresponding to ADDRESS.
 	volatile uint32_t *target_virtual_addr = page_virtual_addr + offset_in_page/sizeof(uint32_t *);
-	//printf("target_virtual_addr = %p\n", target_virtual_addr);
-	//printf("-----------------------------------------------------------------------------\n");
 	
-	if(is_write){
-		const uint32_t VALUE = strtoul(val, NULL, 0);
-		*target_virtual_addr = VALUE;
-	}
-	else{
-		//printf("\nvalue at 0x%x = 0x%x\n", ADDRESS, *target_virtual_addr);
-	}
+	const uint32_t VALUE = strtoul(val, NULL, 0);
+	*target_virtual_addr = VALUE;
 	return 0;
 }
 
 void when_exit(){
 	devmem("0xff200000", "0x0");
+	exit(0);
 }
 
 int main(int argc, char **argv){
 	signal(2, when_exit);
-	devmem("0xff200000","0x1");
-	usage();
-	dvmemusage();
-	char n;
-	scanf("%c", &n);
 	atexit(when_exit);
+	devmem("0xff200000","0x1");
+	int c;
+	bool verbose = false;
+	bool termpats = false;
+	bool filepats = false;
+	if((c = getopt(argc, argv, "hvpf")) == -1){
+		fprintf(stderr, "Please specify options");
+		usage();
+	}
+	while (c != -1){ // Get the options inputed and set variables to reflect the ones chosen
+		switch(c){
+			case 'h':
+				usage();
+				exit(0);
+				break;
+			case 'v':
+				verbose = true;
+				break;
+			case 'p':
+				termpats = true;
+				break;
+			case 'f':
+				filepats = true;
+				break;
+			default:
+				printf("?? getopt returned character code %o ??\n", c);
+				fprintf(stderr, "Please specify options");
+				usage();
+				break;
+		}
+		c = getopt(argc, argv, "hvpf");
+	}
+	
+	if(termpats && filepats){ //handle both p & f options
+		fprintf(stderr, "\n\tCannot use both -p and -f options in the same exceution");
+		exit(0);
+	}
+	if(termpats){
+		int arraysize = argc - optind;
+		char patterns[arraysize][10] = {};
+		//optind++;
+		while(optind < argc){
+			for(int i = 0; i < sizeof(argv[optind]); i++){
+				patterns[(argc - optind)-1][i] = argv[(argc - optind)+1][i];
+			}
+			//printf("%d %d\n", optind, argc);
+			optind++;
+		}
+		for(int i = 0; i < arraysize; i++){
+			printf("%s\n", patterns[i]);
+		}
+	}
+	
+	
+	return 0;
 }
 
