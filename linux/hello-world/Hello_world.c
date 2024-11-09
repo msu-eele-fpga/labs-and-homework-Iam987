eba9663036a6c32d2a4154c5c774a03163dcb42a
Ian Crittenden <iancrittenden@rocketmail.com>
2024-11-08 23:33:05 -0700

    HW 8 - Get ready for cross-compile

--- linux/hello-world/Hello_world.c
+++ linux/hello-world/Hello_world.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+#define MyName "Ian Crittenden"
+
 static int __init init_hello(void){
 	printk(KERN_INFO "Hello, World\n");
 	return 0;
@@ -10,5 +12,8 @@ static void __exit cleanup_hello(void){
 	printk(KERN_INFO "Goodbye, cruel world");
 }
 
+module_init(init_hello);
+module_exit(cleanup_hello);
 
-#define MODULE_AUTHOR("Ian Crittenden")
+MODULE_AUTHOR(MyName);
+MODULE_LICENSE("GPL");
